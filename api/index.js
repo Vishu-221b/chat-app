a2caaa0b73bbf8794d2b992a03adc1e2cf2a828b
vishu <vishwajeetsinghchauhan63@gmail.com>
2023-11-28 21:53:32 +0530

    scripts added to run the server

--- api/index.js
+++ api/index.js
@@ -1,3 +1,9 @@
 import express from 'express';
 
 const app = express();
+
+const port = 3000;
+
+app.get('/', (req, res) => { res.send ('Hello people, Welcome to my world.')});
+
+app.listen(port, () => console.log("The server is running."));
