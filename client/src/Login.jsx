10c85cd3e765415dfe7e2ea35c6269fc8ce8a188
vishu <vishwajeetsinghchauhan63@gmail.com>
2023-12-02 11:11:39 +0530

    Login client form completed

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -1,6 +1,17 @@
+import {Link} from "react-router-dom";
+
 function Login(){
     return(
-        <div>hiii</div>
+        <div className="bg-blue-500 min-h-screen flex flex-col justify-center items-center">
+        <form className="flex flex-col">
+
+            <input className="p-4 mt-4" type="text" placeholder="username"/>
+            <input className="p-4 mt-4" type="password" placeholder="password"/>
+            <button className="p-4 mt-4 bg-orange-400">LOG IN</button>
+
+        </form>
+        </div>
+        
     )
 }
 
--- client/src/Register.jsx
+++ client/src/Register.jsx
@@ -22,15 +22,15 @@ function Register() {
       <h1 className="text-2xl text-white ">Sign Up</h1>
       
       <form className="flex flex-col" onSubmit = {register} >
-        <input value={username} onChange={ev => setUsername(ev.target.value)} className= "mt-4 p-2" type="text" placeholder="username"/>
+        <input value={username} onChange={ev => setUsername(ev.target.value)} className= "mt-4 p-4" type="text" placeholder="username"/>
         
-        <input value={password} onChange={ev => setPassword(ev.target.value)} className= "mt-4 p-2" type="password" placeholder="Enter your password"/>
+        <input value={password} onChange={ev => setPassword(ev.target.value)} className= "mt-4 p-4" type="password" placeholder="Enter your password"/>
   
-        <button type="submit" className= "mt-4 p-2 bg-orange-300">Register</button>
+        <button type="submit" className= "mt-4 p-4 bg-orange-300">Register</button>
 
       </form>
 
-      <Link to="/login"><button className="w-[400px] bg-orange-400 mt-4 p-2">Already have an account? Login</button></Link>
+      <Link to="/login"><button className="w-[400px] bg-orange-400 mt-4 p-4">Already have an account? Login</button></Link>
   
       </div>
     )
